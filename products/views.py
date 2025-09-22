615bce0c7157f1b6d6ce1d0c6acfbe901d267f75
Aleksey Baranov <kafaki220@gmail.com>
2025-09-22 10:44:38 +0300

    feat: add templates, view func

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request, template_name='products/index.html', )
+
+
+def products(request):
+    return render(request, template_name='products/products.html', )
