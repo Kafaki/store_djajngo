65b6fa0388ba82eb294995b2c0c0db0e03b1b68f
Aleksey Baranov <kafaki220@gmail.com>
2025-09-23 14:13:59 +0300

    feat: add login_user

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login_user, name='login'),
     path('products/', views.registration, name='registration'),
+
+    path('logout/', LogoutView.as_view(next_page='products:index'), name='logout'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+from users.forms import UserLoginForm
+
 
 # Create your views here.
 
-def login(request):
-    return  render(request, template_name='users/login.html', )
+def login_user(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user and user.is_active:
+                print('done')
+                login(request, user)
+                return redirect('products:index')
+    else:
+        form = UserLoginForm()
+
+    return render(request, template_name='users/login.html', context={'form': form})
+
+
 
 
 def registration(request):
-    return  render(request, template_name='users/registration.html', )
+    return render(request, template_name='users/registration.html', )
