7e45623731f6d61d880ebe7e268559ca235d8727
Aleksey Baranov <kafaki220@gmail.com>
2025-09-28 14:13:08 +0300

    feat: add UserLoginView

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('login/', views.login_user, name='login'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
     path('registration/', views.UserRegistrationView.as_view(), name='registration'),
 
     path('profile/', views.UserProfileView.as_view(), name='profile'),
--- users/views.py
+++ users/views.py
@@ -1,28 +1,33 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
-from products.models import Basket
+# from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import messages
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return redirect('products:index')
-    else:
-        form = UserLoginForm()
-
-    return render(request, template_name='users/login.html', context={'form': form})
+class UserLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
+    next_page = reverse_lazy('products:index')
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'], password=cd['password'])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return redirect('products:index')
+#     else:
+#         form = UserLoginForm()
+#
+#     return render(request, template_name='users/login.html', context={'form': form})
 
 
 class UserRegistrationView(CreateView):
