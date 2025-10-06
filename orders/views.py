9775627f2fc771d66fe0e7c1416061e5d99de03e
Aleksey Baranov <kafaki220@gmail.com>
2025-10-06 22:44:52 +0300

    feat: add OrderCreateView

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from orders.forms import OrderForm
@@ -6,3 +7,11 @@ from orders.forms import OrderForm
 class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+    extra_context = {'title': "Store - оформление заказа"}
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return  super().form_valid(form)
+
+
