93b74aa6b40ac21ed05420336fd574a7491cd53c
Aleksey Baranov <kafaki220@gmail.com>
2025-10-07 16:08:09 +0300

    feat: add Stripe(CheckoutSession,  connect webhook)

--- orders/urls.py
+++ orders/urls.py
@@ -6,5 +6,7 @@ app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('order-success/', views.SuccessTemplateView.as_view(), name='order_success'),
+    path('order-canceled/', views.CancelTemplateView.as_view(), name='order_canceled'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,25 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from http import HTTPStatus
+
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import CreateView, TemplateView
 
 from orders.forms import OrderForm
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class SuccessTemplateView(TemplateView):
+    template_name = 'orders/success.html'
+    extra_context = {"title": "Store - Спасибо за заказ!"}
+
+
+class CancelTemplateView(TemplateView):
+    template_name = 'orders/success.html'
 
 
 class OrderCreateView(CreateView):
@@ -10,8 +28,62 @@ class OrderCreateView(CreateView):
     success_url = reverse_lazy('orders:order_create')
     extra_context = {'title': "Store - оформление заказа"}
 
+    def post(self, request, *args, **kwargs):
+        super().post(request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    # Provide the exact Price ID (for example, price_1234) of the product you want to sell
+                    'price': 'price_1SFVpPQhVZkj308BoHoECEmB',
+                    'quantity': 2,
+                },
+            ],
+            metadata = {'order_id': self.object.id},
+            mode='payment',
+            success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
+            cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_canceled')),
+        )
+        return redirect(checkout_session.url, status=HTTPStatus.SEE_OTHER)
+
     def form_valid(self, form):
         form.instance.initiator = self.request.user
-        return  super().form_valid(form)
+        return super().form_valid(form)
 
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fulfill the purchase...
+        fulfill_order(session)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
+
+
+def fulfill_order(session):
+    # Проверяем наличие order_id в metadata
+    if not session.metadata or 'order_id' not in session.metadata:
+        print(f"⚠️ Order ID not found in session metadata. Session ID: {session.id}")
+        return
+
+    order_id = int(session.metadata.order_id)
+    print("WHats doing!!!")
 
